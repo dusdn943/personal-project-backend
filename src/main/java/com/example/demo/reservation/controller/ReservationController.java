1b662744bd93f021b2b91099f90f6da82d6a8e6e
dusdn943 <128335427+dusdn943@users.noreply.github.com>
2023-08-01 16:56:42 +0900

    feat: [최연우] 레스토랑의 상세 정보를 확인할 수 있습니다 [SCP-9]

--- src/main/java/com/example/demo/reservation/controller/ReservationController.java
+++ src/main/java/com/example/demo/reservation/controller/ReservationController.java
@@ -18,6 +18,7 @@ import com.example.demo.reservation.dto.ReservationResponse;
 import com.example.demo.reservation.service.ReservationService;
 import com.example.demo.restaurant.service.RestaurantService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -28,8 +29,9 @@ public class ReservationController {
     private final RestaurantService restaurantService;
     private final ReservationService reservationService;
 
-    // 전제 : 사용자가 몇명 예약할지를 선택해야 할 거 같다.
-    // API : 사용자는 달력의 특정 날짜, 예약 인원을 선택해 해당 Restaurant 의 예약 가능한 시간을 확인할 수 있다.
+    // 전제 : 사용자가 몇명 예약할지를 선택해야할거같음
+    // API : 사용자는 달력의 특정 날짜, 예약 인원을 선택해 해당 Restaurant 의 예약가능한 시간을 확인할 수 있다.
+    @Operation(summary = "레스토랑의 현재 이용가능한 예약시간 조회하는 API")
     @GetMapping("/restaurant/{id}/available-reservation") // 사용자로부터 받아야 할 데이터 : 언제(날짜)
     public List<LocalTime> retrieveAvailableForReservation(
             @PathVariable(name = "id") long restaurantId,
@@ -41,6 +43,7 @@ public class ReservationController {
         // [12:00, 13:00, 14:00, 15:00, 19:00]
     }
 
+    @Operation(summary = "해당 멤버가 예약한 예약정보 전체 조회 API")
     @GetMapping("/reservations/member/{id}")
     public List<ReservationResponse> retrieveAllReservationByMember(@PathVariable(name = "id") long memberId) {
         System.out.println("memberId = " + memberId);
@@ -48,17 +51,19 @@ public class ReservationController {
     }
 
     // API : 사용자는 Restaurant 을 예약할 수 있다.
+    @Operation(summary = "레스토랑을 예약하는 API")
     @PostMapping("/restaurant/{id}/reservation")
     public void reservation(
             @PathVariable(name = "id") long restaurantId,
             @RequestBody ReservationRequest req
     ) {
-        // restaurantId(어디), req(누가, 언제, 몇명) -> 하나의 객체로 묶어보겠다.
-        // 그러면 당연히 이 묶인 객체도 DTO
+        // restaurantId(어디), req(누가, 언제, 몇명) -> 하나의 객체로 묶음
+        // 그러면 당연히 이 묶인 객체도 DTO임
         ReservationCommand command = new ReservationCommand(restaurantId, req);
         reservationService.reservation(command);
     }
 
+    @Operation(summary = "레스토랑 예약을 취소 하는 API")
     @DeleteMapping("/reservation/{id}")
     public void cancelReservation(@PathVariable(name = "id") long reservationId) {
         reservationService.cancel(reservationId);
--- src/main/java/com/example/demo/restaurant/controller/RestaurantController.java
+++ src/main/java/com/example/demo/restaurant/controller/RestaurantController.java
@@ -2,6 +2,7 @@ package com.example.demo.restaurant.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.restaurant.dto.RegisterRestaurantRequest;
+import com.example.demo.restaurant.dto.RestaurantDetailResponse;
 import com.example.demo.restaurant.dto.RestaurantResponse;
 import com.example.demo.restaurant.service.RestaurantService;
 
@@ -21,24 +23,44 @@ public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
+    @Operation(summary = "전체 맛집 목록을 조회하는 API")
+    @GetMapping("/restaurants")
+    public List<RestaurantResponse> retrieveAllRestaurants() {
+        return restaurantService.retrieveAll();
+    }
+
+    // API : 사용자는 Restaurant 의 상세정보를 확인할 수 있다.
+    @Operation(summary = "레스토랑 상세 정보를 확인하는 API")
+    @GetMapping("/restaurant/{id}") // 이런 방식의 데이터 전달 방식 => Path Variable
+    public RestaurantDetailResponse retrieveRestaurantDetail(@PathVariable(name = "id") long restaurantId) {
+        System.out.println("restaurantId = " + restaurantId);
+        return restaurantService.retrieveDetail(restaurantId);
+    }
+
+    @Operation(summary = "사업자 회원에 대한 모든 레스토랑 정보를 확인하는 API")
+    @GetMapping("/restaurants/business-member/{id}")
+    public List<RestaurantResponse> retrieveRestaurantByBusinessMember (
+            @PathVariable(name = "id") long businessMemberId
+    ) {
+        System.out.println("businessMemberId = " + businessMemberId);
+        return restaurantService.retrieveAll(businessMemberId);
+    }
+
+    // 맛집 등록을 수정해야함 (정보 추가)
+    //  -> 영업일(business_days), 영업시간(opening_at, closing_at), 시간당 예약가능 인원(reservation_capacity)
+    @Operation(summary = "레스토랑 상세 정보 등록하는 API")
     @PostMapping("/restaurant")
     public void registerRestaurant(@RequestBody RegisterRestaurantRequest req) {
         System.out.println(req);
         restaurantService.register(req);
     }
 
+    @Operation(summary = "레스토랑을 삭제하는 API")
     @DeleteMapping("/restaurant/{id}")
     public void deleteRestaurant(@PathVariable(name = "id") long restaurantId) {
         System.out.println("restaurantId = " + restaurantId);
         restaurantService.delete(restaurantId);
     }
 
-    @GetMapping("/restaurants/business-member/{id}")
-    public List<RestaurantResponse> retrieveRestaurantByBusinessMember(
-            @PathVariable(name = "id") long businessMemberId
-    ) {
-        System.out.println("businessMemberId = " + businessMemberId);
-        return restaurantService.retrieveAll(businessMemberId);
-    }
 }
 
